bf1b4eb721ae77874a7cb18c37497dd044b70916
Mateus GP <78314156+mateusgsp@users.noreply.github.com>
2022-06-19 21:18:32 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,8 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit: '2mb'}));
-app.use(express.urlencoded({limit: '2mb'}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
